e1008cea8f2e970e2b026b08db1075197e0e727d
lizard43 <lizard43@gmail.com>
2026-08-24 15:30:27 -0500

    build scripts update

--- build.bat
+++ build.bat
@@ -1,22 +1,37 @@
 @echo off
 setlocal
 
-set "ZMAC_BIN=zmac"
-if defined ZMAC set "ZMAC_BIN=%ZMAC%"
-
-"%ZMAC_BIN%" src\seawolf2_disassembly.asm
+echo Sea Wolf II ROM build
+echo Input: src\seawolf2.asm
+echo.
+echo [1/4] Assemble the 8 KB program image
+echo ^> tools\zmac.exe -o src\seawolf2.bin src\seawolf2.asm
+tools\zmac.exe -o src\seawolf2.bin src\seawolf2.asm
 if errorlevel 1 exit /b 1
+echo Created: src\seawolf2.bin
+echo Created: src\seawolf2.lst
 
+echo.
+echo [2/4] Split the image into four 2 KB ROMs
 if not exist roms mkdir roms
-
-powershell -NoProfile -Command "$b=[IO.File]::ReadAllBytes('src\seawolf2_disassembly.bin'); 0..3 | ForEach-Object { [IO.File]::WriteAllBytes(('roms\sw2x{0}.bin' -f ($_+1)), $b[($_*2048)..(($_*2048)+2047)]) }"
+powershell -NoProfile -Command "$b=[IO.File]::ReadAllBytes('src\seawolf2.bin'); 0..3 | ForEach-Object { [IO.File]::WriteAllBytes(('roms\sw2x{0}.bin' -f ($_+1)), $b[($_*2048)..(($_*2048)+2047)]) }"
 if errorlevel 1 exit /b 1
+echo Created: roms\sw2x1.bin
+echo Created: roms\sw2x2.bin
+echo Created: roms\sw2x3.bin
+echo Created: roms\sw2x4.bin
 
+echo.
+echo [3/4] Package the MAME ROM set
+echo ^> powershell Compress-Archive ... roms\seawolf2.zip
 powershell -NoProfile -Command "Compress-Archive -Force -Path roms\sw2x1.bin,roms\sw2x2.bin,roms\sw2x3.bin,roms\sw2x4.bin -DestinationPath roms\seawolf2.zip"
 if errorlevel 1 exit /b 1
+echo Created: roms\seawolf2.zip
 
-certutil -hashfile roms\sw2x1.bin SHA1
-certutil -hashfile roms\sw2x2.bin SHA1
-certutil -hashfile roms\sw2x3.bin SHA1
-certutil -hashfile roms\sw2x4.bin SHA1
+echo.
+echo [4/4] SHA1
+powershell -NoProfile -Command "'roms\sw2x1.bin','roms\sw2x2.bin','roms\sw2x3.bin','roms\sw2x4.bin' | ForEach-Object { $h=(Get-FileHash $_ -Algorithm SHA1).Hash.ToLower(); '{0}  {1}' -f $h,$_ }"
+if errorlevel 1 exit /b 1
 
+echo.
+echo Build complete: roms\seawolf2.zip
--- build.sh
+++ build.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Sea Wolf II ROM build"
+echo "Input: src/seawolf2.asm"
+echo
+echo "[1/4] Assemble the 8 KB program image"
+echo "+ ./tools/zmac -o src/seawolf2.bin src/seawolf2.asm"
 ./tools/zmac -o src/seawolf2.bin src/seawolf2.asm
+echo "Created: src/seawolf2.bin"
+echo "Created: src/seawolf2.lst"
 
+echo
+echo "[2/4] Split the image into four 2 KB ROMs"
 mkdir -p roms
-
 dd if=src/seawolf2.bin of=roms/sw2x1.bin bs=2048 skip=0 count=1 status=none
 dd if=src/seawolf2.bin of=roms/sw2x2.bin bs=2048 skip=1 count=1 status=none
 dd if=src/seawolf2.bin of=roms/sw2x3.bin bs=2048 skip=2 count=1 status=none
 dd if=src/seawolf2.bin of=roms/sw2x4.bin bs=2048 skip=3 count=1 status=none
+echo "Created: roms/sw2x1.bin"
+echo "Created: roms/sw2x2.bin"
+echo "Created: roms/sw2x3.bin"
+echo "Created: roms/sw2x4.bin"
 
+echo
+echo "[3/4] Package the MAME ROM set"
+echo "+ zip -jqFS roms/seawolf2.zip roms/sw2x1.bin roms/sw2x2.bin roms/sw2x3.bin roms/sw2x4.bin"
 zip -jqFS roms/seawolf2.zip roms/sw2x1.bin roms/sw2x2.bin roms/sw2x3.bin roms/sw2x4.bin
+echo "Created: roms/seawolf2.zip"
+
+echo
+echo "[4/4] SHA1"
 sha1sum roms/sw2x1.bin roms/sw2x2.bin roms/sw2x3.bin roms/sw2x4.bin
+
+echo
+echo "Build complete: roms/seawolf2.zip"
